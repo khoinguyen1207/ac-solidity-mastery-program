934884f838cceae5a7d1a0c22c92e3d59b51223b
Khoi Nguyen <khoinguyentpvl@gmail.com>
2024-09-06 13:44:36 +0700

    init token drop

--- final-project/token-drop/contracts/Lock.sol
+++ final-project/token-drop/contracts/Lock.sol
@@ -2,19 +2,13 @@
 pragma solidity ^0.8.24;
 
 // Uncomment this line to use console.log
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
     address payable public owner;
 
-    error InvalidUnlockTime(uint unlockTime, uint currentTime);
     event Withdrawal(uint amount, uint when);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You aren't the owner");
-        _;
-    }
 
     constructor(uint _unlockTime) payable {
         require(
@@ -26,11 +20,12 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public onlyOwner {
+    function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        if (block.timestamp < unlockTime) {
-            revert InvalidUnlockTime(unlockTime, block.timestamp);
-        }
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
